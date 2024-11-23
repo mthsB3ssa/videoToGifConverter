a2d225c2d66b265470073eb6e12ae17602f6ea1a
Matheus <mthsbessac@gmail.com>
2024-11-23 15:00:42 -0300

    setting start and end time, and fps

--- main.go
+++ main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
+    "fmt"
+
     ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
-func VideoToGIF(inputPath, outputPath string) error {
-    return ffmpeg.Input(inputPath).
+func VideoToGIF(inputPath, outputPath string, startTime, endTime float64, fps int) error {
+    duration := endTime - startTime
+
+    return ffmpeg.Input(inputPath, ffmpeg.KwArgs{
+        "ss": fmt.Sprintf("%f", startTime),
+    }).
         Output(outputPath, ffmpeg.KwArgs{
-            "vf":   "fps=10,scale=320:-1:flags=lanczos",
-            "loop": 0,
-            "ss":   "0",
-            "t":    "10",
+            "vf": fmt.Sprintf("fps=%d,scale=320:-1:flags=lanczos", fps),
+            "t":  fmt.Sprintf("%f", duration),
         }).
         OverWriteOutput().
         Run()
 }
 
 func main() {
-    err := VideoToGIF("input.mp4", "output.gif")
+    // Exemplo de uso:
+    // Converter de 5.0 segundos a 15.0 segundos, com 15 fps
+    err := VideoToGIF("input.mp4", "output.gif", 5.0, 10.0, 15)
     if err != nil {
         panic(err)
     }
